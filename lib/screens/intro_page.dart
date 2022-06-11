65924a13df168d9153ecc5cce3d79da090e29114
retro-dane <43159871+retro-dane@users.noreply.github.com>
2022-06-10 20:59:57 -0500

    Initial commit

---
+++ lib/screens/intro_page.dart
@@ -0,0 +1,82 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:login/widgets/register_signup_button.dart';
+
+class Intro extends StatelessWidget {
+  const Intro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration:  BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.center,
+              end: Alignment.bottomCenter,
+              stops: const [
+            0.2,
+            0.9
+          ], colors: [
+            Colors.white.withOpacity(0.8),
+            Colors.purple.withOpacity(0.7)
+          ])
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.white.withOpacity(0.85),
+        body: Column(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 30),
+                child: Container(
+                    width:double.infinity,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: const DecorationImage(
+                      filterQuality: FilterQuality.high,
+                      image: AssetImage(
+                          "assets/images/brain.jpg",),
+                          fit: BoxFit.cover
+                      )
+                    )
+                  ),
+              ),
+            ),
+            const Text("Discover the World.\nRight Now.",
+              softWrap: true,
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.black,
+                fontWeight:FontWeight.w600,
+                decoration: TextDecoration.none,
+              ),
+            ),
+            const SizedBox(height:20),
+            const Text("Explore all the greatness this life has to offer\nbased on your personality",
+              softWrap: true,
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 13,
+                color: Colors.grey,
+                fontWeight:FontWeight.w600,
+                decoration: TextDecoration.none,
+              ),
+            ),
+            const SizedBox(height:40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                RegisterSignIn(color: Colors.white,title:"Register",),
+                RegisterSignIn(color: Colors.transparent,title:"Sign In"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/register_signup_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class RegisterSignIn extends StatelessWidget {
+  const RegisterSignIn({Key? key,
+    required this.color,
+    required this.title,
+    this.onpressed
+  }) : super(key: key);
+
+  final Color color;
+  final String title;
+  final void Function()? onpressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onpressed,
+      child: Container(
+        width: 130,
+        height: 50,
+        decoration: BoxDecoration(
+          color: color,
+          border: Border.all(width:3,color: Colors.white),
+        ),
+        child:  Center(
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
